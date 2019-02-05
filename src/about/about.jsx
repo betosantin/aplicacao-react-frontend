b4fb6b1f182d4981511e6abe28d76227d028dfc7
Robertto Santin <betosantin@gmail.com>
2019-02-05 17:27:44 -0200

    continuacao da implementacao

--- src/about/about.jsx
+++ src/about/about.jsx
@@ -4,7 +4,7 @@ import PageHeader from '../template/pageHeader'
 
 export default props => (
     <div>
-        <PageHeader name='Sobre' small='Nós'></PageHeader>
+        <PageHeader name='Sobre' small=' Nós'></PageHeader>
         <h2>Nossa História</h2>
         <p>Lorem ipsum dolor sit amet..</p>
         <h2>Missão e Visão</h2>
